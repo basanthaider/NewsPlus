79600ca830fe8d2391edde267dfa95ed8881acf9
BaSant <basssnthaider@gamil.com>
2024-09-03 15:17:27 +0300

    Adding the Api and the models Needed for it

--- app/src/main/java/com/example/newsapp/api/NewsCallable.kt
+++ app/src/main/java/com/example/newsapp/api/NewsCallable.kt
@@ -1,4 +1,22 @@
 package com.example.newsapp.api
 
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.util.Constants.Companion.API_KEY
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsCallable {
+    @GET("/v2/top-headlines")
+   suspend fun getNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Call<NewsResponse>
+
+
+
 }
